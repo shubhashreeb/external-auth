977831c3d09a9cffdbf59e947ebed05ef5f16d24
Shubhashree <shubhashree100@gmail.com>
2023-05-28 13:58:39 -0700

    init code

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/shubhashreeb/external-auth
+
+go 1.20
+
+require (
+	github.com/envoyproxy/go-control-plane v0.11.0
+	github.com/gogo/googleapis v1.4.1
+	golang.org/x/net v0.10.0
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230526203410-71b5a4ffd15e
+	google.golang.org/grpc v1.55.0
+)
+
+require (
+	github.com/cncf/xds/go v0.0.0-20230310173818-32f1caf87195 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.10.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	"github.com/gogo/googleapis/google/rpc"
+	"golang.org/x/net/context"
+	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc"
+)
+
+type AuthorizationServer struct{}
+
+func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
+	log.Println(">>> Server Performing authorization check!")
+	method := req.Attributes.Request.Http.Method
+	path := req.Attributes.Request.Http.Path
+	authHeader, ok := req.Attributes.Request.Http.Headers["authorization"]
+
+	if !ok {
+		fmt.Println("failed to receive headers")
+	}
+
+	fmt.Println("Here are the request headers", req)
+	fmt.Println("Here are the request info", method, path, authHeader)
+
+	return &auth.CheckResponse{
+		Status: &rpcstatus.Status{
+			Code: int32(rpc.OK),
+		},
+		HttpResponse: &auth.CheckResponse_OkResponse{
+			OkResponse: &auth.OkHttpResponse{
+				Headers: []*core.HeaderValueOption{
+					{
+						Header: &core.HeaderValue{
+							Key:   "x-prism-authorized",
+							Value: "allowed",
+						},
+					},
+				},
+			},
+		},
+	}, nil
+
+	/*
+		return &auth.CheckResponse{
+					Status: &rpcstatus.Status{
+						Code: int32(rpc.PERMISSION_DENIED), // Code: int32(rpc.UNAUTHENTICATED),
+					},
+					HttpResponse: &auth.CheckResponse_DeniedResponse{
+						DeniedResponse: &auth.DeniedHttpResponse{
+							Status: &envoy_type.HttpStatus{
+								Code: envoy_type.StatusCode_Unauthorized,
+							},
+							Body: "PERMISSION_DENIED",
+						},
+					},
+				}, nil
+	*/
+
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	auth.RegisterAuthorizationServer(s, &AuthorizationServer{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
